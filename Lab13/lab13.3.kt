cd8088b06ea17828f15f48bd04a2c50ef0713598
O_lesya <74838979+Lesya0723@users.noreply.github.com>
2021-04-15 21:56:09 +0300

    Задание 5

--- Lab13/lab13.3.kt
+++ Lab13/lab13.3.kt
@@ -4,4 +4,32 @@ fun main(args: Array<String>) {
     if (name != null) {
             println("Hello,$name ")
         }
+    println("Какой язык у вас самый любимый? Kotlin, Prolog,C, Java, Python,C#,C++, Php, Pascal или Ruby")
+    val language = readLine()
+    val kotlin  = "Kotlin"
+    val prolog  = "Prolog"
+    val c     = "C"
+    val java  = "Java"
+    val python  = "Python"
+    val csharp  = "C#"
+    val cplusplus  = "C++"
+    val php    = "PHP"
+    val pascal  = "Pascal"
+    val ruby  = "Ruby"
+    if (language != null) {
+        when(language)
+        {
+            kotlin ->  println("Ты подлиза)\n")
+            prolog ->  println("Ты подлиза)\n")
+            c -> println("Ты староват\n")
+            java   -> println("Ты начал с  нужного языка \n")
+            python -> println("Ты современный\n")
+            csharp -> println("Ты что-то можешь \n")
+            cplusplus -> println("Ты первокурсник \n")
+            php -> println("Ты web-designer\n")
+            pascal -> println("Ты учишься в школе \n")
+            ruby ->  println("Ты занимаешь веб-разработкой \n")
+            else -> println("Введи правильно название языка\n")
+        }
+    }
 }
