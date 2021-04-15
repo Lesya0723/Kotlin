e815c26ddc6c7f85f90ed62961d5e71bc9f98433
O_lesya <74838979+Lesya0723@users.noreply.github.com>
2021-04-15 23:11:21 +0300

    Задание 3  Cумма цифр

--- Lab14/ex3.kt
+++ Lab14/ex3.kt
@@ -1,6 +1,12 @@
 fun proizUp(num: Int): Int = if (num < 10) num else proizUp(num/10)*(num%10)
 fun proizDown(num: Int): Int = proizDown(num, 1)
 tailrec fun proizDown(num: Int, p: Int): Int = if (num <10) p*num else proizDown(num/10,p*(num%10))
+
+fun sumUp(num: Int): Int = if (num == 0) num else sumUp(num/10)+num%10
+fun sumDown(num: Int): Int = sumDown(num, 0)
+tailrec fun sumDown(num: Int, sum: Int): Int = if(num == 0) sum else sumDown(num/10,sum+num%10)
+
+
 fun main01()
 {
     println("Введите число")
@@ -10,5 +16,9 @@ fun main01()
     println(proizUp(x))
     print("Произведение цифр 2:" )
     println(proizDown(x))
+    print("Сумма цифр 1:" )
+    println(sumUp(x))
+    print("Сумма цифр  2:" )
+    println(sumDown(x))
 }
 }
