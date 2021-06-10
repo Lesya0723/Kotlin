7bab67a1df522b95f18f91f94f988c9800aba495
O_lesya <74838979+Lesya0723@users.noreply.github.com>
2021-06-10 23:18:00 +0300

    Update lab16.pl

--- Lab16/lab16.pl
+++ Lab16/lab16.pl
@@ -47,3 +47,51 @@ fun ex2_8(s: String): Int {
     return if (s == "") 0
     else listOfWords.count { it -> it.count() % 2 == 0 }
 }
+
+
+fun findDates(s: String): List<String> {
+  
+    fun numberOfDays(month: String, year: Int): Int =
+        when {
+            (month == "января") -> 31
+            (month == "февраля") && (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0)) -> 29
+            (month == "февраля") -> 28
+            (month == "марта") -> 31
+            (month == "апреля") -> 30
+            (month == "мая") -> 31
+            (month == "июня") -> 30
+            (month == "июля") -> 31
+            (month == "августа") -> 31
+            (month == "сентября") -> 30
+            (month == "октября") -> 31
+            (month == "ноября") -> 30
+            (month == "декабря") -> 31
+            else -> throw DataFormatException("Month isn't in range from 1 to 12")
+        }
+    val text = " $s "
+
+    val checkingDay = "(([1-9])|([1-2][0-9])|(3[0-1]))"
+
+    val months = listOf(
+        "декабря", "января", "февраля",
+        "марта", "апреля", "мая",
+        "июня", "июля", "августа",
+        "сентября", "октября", "ноября"
+    )
+    val checkingMonth = "(${months.joinToString("|")})"
+
+
+    val checkingYear = "((1[0-9]{3})|(20[0-1][0-9])|(202[0-1]))"
+
+  
+    val regexDates = """\s${checkingDay}\s${checkingMonth}\s${checkingYear}\s""".toRegex()
+
+    val listOfDates1 = regexDates.findAll(text).map { it.value.drop(1).dropLast(1).split(" ") }.toList()
+
+  
+    val listOfDates2 = listOfDates1.filter { it[0].toInt() <= numberOfDays(it[1], it[2].toInt())}
+
+
+   
+    return listOfDates2.map { it.joinToString(" ") }
+}
